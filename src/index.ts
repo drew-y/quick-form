6ceb40e0dd29c89f09e9439383beae161245da2e
Drew Youngwerth <drew@youngwerth.com>
2018-08-20 11:55:10 -0600

    Remove file from index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./definitions";
-export * from "./field-templates";
 export * from "./vue-components";
 export * from "./quick-form";
 export * from "./vue-components/quick-form-component";
